759c1418a29087940b2173eb9ae95eedd4ff3e78
JIHYUN <jhjung1402@gmail.com>
2022-08-12 17:02:11 +0900

    Time: 640 ms (62.85%), Space: 15.5 MB (75.57%) - LeetHub

--- 875-koko-eating-bananas/875-koko-eating-bananas.py
+++ 875-koko-eating-bananas/875-koko-eating-bananas.py
@@ -3,15 +3,18 @@ class Solution:
         start = 1
         end = max(piles)
 
-        
         while start <= end:
             speed = (start + end) // 2
             
-            hour = sum([ceil(pile / speed) for pile in piles])
-                 
-            if hour <= h:
-                end = speed - 1
+            hour = 0
+            for pile in piles:
+                hour += ceil(pile / speed)
+                
+                if hour > h:
+                    start = speed + 1
             else:
-                start = speed + 1
+                if hour <= h:
+                    end = speed - 1
+                  
 
         return end + 1
