6982761e8c1497888796376702c118f94c11ecf4
JIHYUN <jhjung1402@gmail.com>
2022-08-22 13:47:29 +0900

    Time: 5103 ms (5.04%), Space: 17.2 MB (97.13%) - LeetHub

--- 318-maximum-product-of-word-lengths/318-maximum-product-of-word-lengths.py
+++ 318-maximum-product-of-word-lengths/318-maximum-product-of-word-lengths.py
@@ -1,6 +1,6 @@
 class Solution:
     def maxProduct(self, words: List[str]) -> int:
-        words = list(set(words))
+        words = list(set(words)) # 단어 중복 제거
         
         _max = 0
         for i in range(0, len(words)):
@@ -8,7 +8,7 @@ class Solution:
             for j in range(i + 1, len(words)):
                 w2 = words[j]
 
-                if not (set(w1) & set(w2)):
+                if not (set(w1) & set(w2)): # 두 단어의 알파벳 교집합이 없는 경우
                     _max = max(_max, len(w1) * len(w2))
         return _max
         
