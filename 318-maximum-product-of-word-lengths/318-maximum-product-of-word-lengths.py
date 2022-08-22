99f98abc4bdbba72853b5faf022f1c41d222dc82
JIHYUN <jhjung1402@gmail.com>
2022-08-22 13:46:40 +0900

    Time: 3166 ms (11.33%), Space: 17.4 MB (55.57%) - LeetHub

--- 318-maximum-product-of-word-lengths/318-maximum-product-of-word-lengths.py
+++ 318-maximum-product-of-word-lengths/318-maximum-product-of-word-lengths.py
@@ -1,28 +1,15 @@
 class Solution:
     def maxProduct(self, words: List[str]) -> int:
-        '''
-        dic = {}
+        words = list(set(words))
+        print(words)
         
-        for word in words:
-            alpha_list = set(word) # 알파벳 중복 제거
-            dic.update({word:alpha_list})
-
-        _max = 0
-        key_list = list(dic.keys())
-        for k1 in key_list[:-1]:
-            for k2 in key_list[1:]:
-                if not (dic[k1] & dic[k2]): # 두 알파벳집합의 교집합이 없는 경우
-                    _max = max(_max, len(k1) * len(k2))
-        
-        return _max
-        '''
         _max = 0
         for i in range(0, len(words)):
             w1 = words[i]
             for j in range(i + 1, len(words)):
                 w2 = words[j]
 
-                if w1 != w2 and not (set(w1) & set(w2)):
+                if not (set(w1) & set(w2)):
                     _max = max(_max, len(w1) * len(w2))
         return _max
         
