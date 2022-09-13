b9c859546db24c04578d5dea2404376abda090fb
JIHYUN <jhjung1402@gmail.com>
2022-09-13 13:53:54 +0900

    Time: 392 ms (20.22%), Space: 18.6 MB (80.92%) - LeetHub

--- 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.py
+++ 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.py
@@ -3,26 +3,8 @@ from heapq import heappush, heappop
 
 class Solution:
     def kthSmallest(self, matrix: List[List[int]], k: int) -> int:
-        nums = sorted(list(itertools.chain.from_iterable(matrix)))
-        length = len(nums)
+        nums = sorted(list(itertools.chain.from_iterable(matrix))) #2차원 배열을 1차원으로 변환
         return nums[k - 1]
-        
-        '''
-        heap = []
-        if k <= length / 2: #최소힙
-            for num in nums:
-                heappush(heap, (num, num))
-        else: #최대힙
-            for num in nums:
-                heappush(heap, (-num, num))
-            k = length - k + 1
-            
-        for _ in range(k - 1):
-            heappop(heap)
-            
-        
-        return heappop(heap)[1]
-        '''
             
             
         
